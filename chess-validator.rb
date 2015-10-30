f34964e468c6e552a7c7487824b955c85334c3cc
Rafael Ramon Fraga <rafael_ramon_fraga@yahoo.es>
2015-10-30 15:21:01 +0100

    All the pieces are done, but we need to refacting things and try to load the kind of piece

--- chess-validator.rb
+++ chess-validator.rb
@@ -1,33 +1,115 @@
 require 'pry'
-class Piece
-  def initialize
+
+module RightMovement
+  def right_movemnt? board, origin, destination
+    orientation = orientation(origin, destination)
+    orientation[0] == 0 || orientation[1] == 0
   end
+end
+
+
+module DiagonalMovement
+  def diagonal_movemnt? board, origin, destination
+    orientation = orientation(origin, destination)
+    orientation[0] == orientation[1]
+  end
+end
+
+
+#------------------------------------------------
+#--------------------PIECES----------------------
+#------------------------------------------------
+
+class Piece
+  def initialize color=:black
+    @color = color
+  end
+
   def orientation origin, destination
     vector = []
     destination.each_with_index do |position, index|
-      vector.push(position - origin[index])
+      vector.push((position - origin[index]).abs)
     end
     vector
   end
+
   def is_empty? board, destination
     board[destination[0]][destination[1]] == nil
   end
 end
 
+
 class Rook < Piece
-  def initialize
-  end
+  include RightMovement
   def check_move? board, origin, destination
     orientation = orientation(origin, destination)
-    if orientation[0] == 0
+    if right_movemnt? board, origin, destination
       is_empty? board, destination
-    elsif orientation[1] == 0
-      is_empty? board, destination
-    else
-      false
     end
   end
 end
+
+class Queen < Piece
+  include RightMovement
+  include DiagonalMovement
+  def check_move? board, origin, destination
+    orientation = orientation(origin, destination)
+    if (right_movemnt? board, origin, destination) || (diagonal_movemnt? board, origin, destination)
+      is_empty? board, destination
+    end
+  end
+end
+
+class Bishop < Piece
+  include DiagonalMovement
+  def check_move? board, origin, destination
+    orientation = orientation(origin, destination)
+    if diagonal_movemnt? board, origin, destination
+      is_empty? board, destination
+    end
+  end
+end
+
+class King < Piece
+  def check_move? board, origin, destination
+    orientation = orientation(origin, destination)
+    if (orientation[0] == 1 || orientation[1] == 1)
+      is_empty? board, destination
+    end
+  end
+end
+
+class Knigth < Piece
+  def check_move? board, origin, destination
+    orientation = orientation(origin, destination)
+    if ( (orientation[0] == 1 &&  orientation[1] == 2) || (orientation[0] == 2 &&  orientation[1] == 1) )
+      is_empty? board, destination
+    end
+  end
+end
+
+class Pawn < Piece
+  def check_move? board, origin, destination
+    orientation = orientation(origin, destination)
+    if( (origin[0] == 1)  && (destination[0] == 3) && (@color == :black))
+      is_empty? board, destination
+    elsif( (origin[0] == 1)  && (destination[0] == 3) && (@color == :white) )
+      is_empty? board, destination
+    elsif( ((destination[0] - origin[0]) == 1) && (@color == :black))
+      is_empty? board, destination
+    elsif((destination[0] - origin[0]) == -1 && (@color == :white))
+      is_empty? board, destination
+    end
+  end
+end
+
+
+
+
+#------------------------------------------------
+#-------------------BOARD------------------------
+#------------------------------------------------
+
 
 class Board
   def initialize pieces, file_name
@@ -71,7 +153,6 @@ class Board
   end
 
   def check_move piece, origin, destination
-    binding.pry
     if @pieces[piece].new.check_move? @board, origin, destination
       puts "LEGAL"
     else
@@ -83,12 +164,22 @@ end
 
 pieces = {
   bR: Rook,
-  wR: Rook
+  wR: Rook,
+  bQ: Queen,
+  wQ: Queen,
+  bB: Bishop,
+  wB: Bishop,
+  bK: King,
+  wK: King,
+  bN: Knigth,
+  wN: Knigth,
+  bP: Pawn,
+  wP: Pawn
 }
-a=[0,0]
-b=[0,7]
+
+
 board = Board.new(pieces, "board_test.txt")
 board.load_board
 board.show_board
-board.check_move(:bR,[0,0],[0,6])
+board.check_move(:bP,[1,0],[3,0])
 
