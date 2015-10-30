cd8534e834221a2b20862635f0d7f5690ae7d318
Rafael Ramon Fraga <rafael_ramon_fraga@yahoo.es>
2015-10-30 11:27:42 +0100

    The boards works and print a txt file

--- chess-validator.rb
+++ chess-validator.rb
@@ -1,2 +1,48 @@
-class Board
+
+class Piece
+  def initialize
+  end
 end
+
+class Rook < Piece
+  def initialize
+  end
+  def check_move board, origin, destination
+
+  end
+end
+
+class Board
+  def initialize file_name
+    @board = Array.new(8, Array.new(8, nil))
+    @file_name = file_name
+  end
+
+  def load_board
+    cell = Array.new(8, Array.new(8, nil))
+    rows = IO.read(@file_name).split("\n")
+    cell = rows.map do |row|
+      row.split
+    end
+    @board = cell
+  end
+
+  def show_board
+    @board.each do |row|
+      row.each do |cell|
+          print cell + " "
+      end
+      puts ""
+    end
+  end
+end
+
+
+pieces = {
+  bR: Rook,
+  wR: Rook
+}
+
+ board = Board.new("board_test.txt")
+ board.load_board
+ board.show_board
