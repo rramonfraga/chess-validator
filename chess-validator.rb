a3c6761d59e8c9bf0f52dc0d8027d56c7dfdab89
Rafael Ramon Fraga <rafael_ramon_fraga@yahoo.es>
2015-10-31 13:47:46 +0100

    The first iteration is correct. It cans read a file board and a file movements and says as if the movements are LEGAL or ILLEGAL. (There aren't colisions include)

--- chess-validator.rb
+++ chess-validator.rb
@@ -93,11 +93,11 @@ class Pawn < Piece
     orientation = orientation(origin, destination)
     if( (origin[0] == 1)  && (destination[0] == 3) && (@color == :black))
       is_empty? board, destination
-    elsif( (origin[0] == 1)  && (destination[0] == 3) && (@color == :white) )
+    elsif( (origin[0] == 6)  && (destination[0] == 4) && (@color == :white) )
       is_empty? board, destination
-    elsif( ((destination[0] - origin[0]) == 1) && (@color == :black))
+    elsif( (orientation[0] == 1 && orientation[1] == 0) && (@color == :black))
       is_empty? board, destination
-    elsif((destination[0] - origin[0]) == -1 && (@color == :white))
+    elsif( (orientation[0] == -1 && orientation[1] == -1) && (@color == :white))
       is_empty? board, destination
     end
   end
@@ -112,11 +112,23 @@ end
 
 
 class Board
-  def initialize pieces, file_name
+  def initialize pieces, positions, file_board, file_movement
     @pieces = pieces
+    @positions = positions
     @board = Array.new(8, Array.new(8, nil))
-    @file_name = file_name
+    @file_board = file_board
+    @file_movement = file_movement
   end
+
+  def load_movements
+    movement = []
+    movements = IO.read(@file_movement).split("\n")
+    new_movements = movements.each do |item|
+      item.split("\n")
+    end
+    new_movements
+  end
+
 
   def nullify_board board
     board.map do |row|
@@ -132,7 +144,7 @@ class Board
 
   def load_board
     cell = Array.new(8, Array.new(8, nil))
-    rows = IO.read(@file_name).split("\n")
+    rows = IO.read(@file_board).split("\n")
     cell = rows.map do |row|
       row.split
     end
@@ -152,34 +164,65 @@ class Board
     end
   end
 
+  def check_color piece
+    if piece[0] == "b"
+      :black
+    else
+      :white
+    end
+  end
+
   def check_move piece, origin, destination
-    if @pieces[piece].new.check_move? @board, origin, destination
+    color = check_color piece
+    #binding.pry
+    if @pieces[piece].new(color).check_move? @board, origin, destination
       puts "LEGAL"
     else
       puts "ILLEGAL"
     end
   end
+
+  def checks_all_the_movements
+    piece = ""
+    movements = load_movements.map do |item|
+      item.split " "
+    end
+    movements.each do |movement|
+      origin = @positions[movement[0].to_sym]
+      destination = @positions[movement[1].to_sym]
+      piece = @board[origin[0]][origin[1]]
+      #binding.pry
+      check_move(piece.to_sym, origin, destination)
+      #binding.pry
+    end
+  end
+
 end
 
 
 pieces = {
-  bR: Rook,
-  wR: Rook,
-  bQ: Queen,
-  wQ: Queen,
-  bB: Bishop,
-  wB: Bishop,
-  bK: King,
-  wK: King,
-  bN: Knigth,
-  wN: Knigth,
-  bP: Pawn,
-  wP: Pawn
+  bR: Rook, wR: Rook, 
+  bQ: Queen, wQ: Queen,
+  bB: Bishop, wB: Bishop,
+  bK: King, wK: King,
+  bN: Knigth, wN: Knigth,
+  bP: Pawn, wP: Pawn
+}
+
+positions = { a8: [0,0], b8: [0,1], c8: [0,2], d8: [0,3], e8: [0,4], f8: [0,5], g8: [0,6], h8: [0,7],
+              a7: [1,0], b7: [1,1], c7: [1,2], d7: [1,3], e7: [1,4], f7: [1,5], g7: [1,6], h7: [1,7],
+              a6: [2,0], b6: [2,1], c6: [2,2], d6: [2,3], e6: [2,4], f6: [2,5], g6: [2,6], h6: [2,7],
+              a5: [3,0], b5: [3,1], c5: [3,2], d5: [3,3], e5: [3,4], f5: [3,5], g5: [3,6], h5: [3,7],
+              a4: [4,0], b4: [4,1], c4: [4,2], d4: [4,3], e4: [4,4], f4: [4,5], g4: [4,6], h4: [4,7],
+              a3: [5,0], b3: [5,1], c3: [5,2], d3: [5,3], e3: [5,4], f3: [5,5], g3: [5,6], h3: [5,7],
+              a2: [6,0], b2: [6,1], c2: [6,2], d2: [6,3], e2: [6,4], f2: [6,5], g2: [6,6], h2: [6,7],
+              a1: [7,0], b1: [7,1], c1: [7,2], d1: [7,3], e1: [7,4], f1: [7,5], g1: [7,6], h1: [7,7]
 }
 
 
-board = Board.new(pieces, "board_test.txt")
+board = Board.new(pieces, positions, "board_test.txt", "movements.txt")
 board.load_board
 board.show_board
-board.check_move(:bP,[1,0],[3,0])
+#board.check_move(:bP,[1,0],[3,0])
+board.checks_all_the_movements
 
